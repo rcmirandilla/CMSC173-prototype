e2844f5a199143f489d5065eb091b6fdf81b83cb
mtlayones <mtlayones@up.edu.ph>
2021-06-06 13:12:25 +0800

    fix codebase

---
+++ src/AppRouter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+const AppRouter = () => {
+	return (
+		<Router>
+			<Switch></Switch>
+		</Router>
+	);
+};
+
+export default AppRouter;
